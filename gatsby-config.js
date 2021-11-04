21093f4aca8969c7b234ee16df63759a849667ea
Thomas Harris <loden16harris@gmail.com>
2021-11-04 10:50:38 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Multiplied By One",
+  },
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"],
+};
